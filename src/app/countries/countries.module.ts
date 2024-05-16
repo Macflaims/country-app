814495f99d26874bd7fd7c505fa0e0fffc5e6a8a
Martin <martinsanchez2533@gmail.com>
2024-05-16 09:07:51 -0300

    agrega routing

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CountriesModule],
   exports: [],
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
+  ],
   providers: [],
 })
 export class CountriesModule { }
