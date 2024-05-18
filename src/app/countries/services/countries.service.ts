779de72dd37b3b040e9e31e715b96b3ef70277d1
Martin <martinsanchez2533@gmail.com>
2024-05-18 13:29:43 -0300

    agrega llamada a api

---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountriesService {
+  constructor(private http: HttpClient) { }
+
+  private apiUrl: string = "https://restcountries.com/v3.1"
+
+  searchCapital(term: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+  }
+
+}
