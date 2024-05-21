934e6d0f2070ee8651d2e76421c0e66dff181fb1
Martin <martinsanchez2533@gmail.com>
2024-05-21 11:53:14 -0300

    agrega loader

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -9,11 +9,16 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCountryPageComponent {
   public countries: Country[] = []
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) { }
 
   searchByCountry(term: string): void {
-    this.countriesService.searchCountry(term).subscribe(countries => this.countries = countries)
+    this.isLoading = true;
+    this.countriesService.searchCountry(term).subscribe(countries => {
+      this.countries = countries
+      this.isLoading = false;
+    })
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,19 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { HomePageComponent } from './pages/home-page/homePage.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RouterModule } from '@angular/router';
-import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-import { SearchBoxComponent } from './components/searchBox/searchBox.component';
 
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { CommonModule } from '@angular/common';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/homePage.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SearchBoxComponent } from './components/searchBox/searchBox.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoadingSpinnerComponent } from './components/loadingSpinner/loadingSpinner.component';
 
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  exports: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent],
-  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent],
+  exports: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent, LoadingSpinnerComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPageComponent, SearchBoxComponent, LoadingSpinnerComponent],
   providers: [],
 })
 export class SharedModule { }
