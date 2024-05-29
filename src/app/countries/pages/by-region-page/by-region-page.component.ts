ca63c92ce136f9841a659e60433378bbb732c0c1
Martin <martinsanchez2533@gmail.com>
2024-05-29 09:05:29 -0300

    mejora funcionalidad de busqueda

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,20 +2,27 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = "Africa" | "Americas" | "Asia" | "Europe" | "Oceania";
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
-  public isLoading: boolean = false
-  public countries: Country[] = []
+  public isLoading: boolean = false;
+  public countries: Country[] = [];
+  public regions: Region[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
+
+    this.selectedRegion = region;
+
     this.isLoading = true;
-    this.countriesService.searchRegion(term).subscribe(countries => {
+    this.countriesService.searchRegion(region).subscribe(countries => {
       this.countries = countries;
       this.isLoading = false;
     })
