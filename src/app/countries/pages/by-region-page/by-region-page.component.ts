971ee1c1a335e1393f21655cfcf32e8788054c9c
Martin <martinsanchez2533@gmail.com>
2024-05-31 10:09:31 -0300

    mantiene información en pantalla

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
 
-type Region = "Africa" | "Americas" | "Asia" | "Europe" | "Oceania";
+
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public isLoading: boolean = false;
   public countries: Country[] = [];
   public regions: Region[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
   public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) { }
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byRegion.countries
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
 
   searchByRegion(region: Region): void {
 
